d2448bf91cf5deed683a90f07b16db8bb5089f99
SergioCabrera <sergiodcr0258@gmail.com>
2021-03-03 19:17:22 -0600

    Creando la solución y el modelo de los items

---
+++ Laboratorio2_ArbolBinario/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Laboratorio2_ArbolBinario.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Laboratorio2_ArbolBinario/Models/Item.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CsvHelper.Configuration.Attributes;
+
+namespace Laboratorio2_ArbolBinario.Models
+{
+    public class Item
+    {
+        [Name("id")]
+        public int Id { get; set; }
+        [Name("name")]
+        public string Name { get; set; }
+        [Name("description")]
+        public string Description { get; set; }
+        [Name("production_house")]
+        public string Production_House { get; set; }
+
+        [Name("price")]
+        public string Price { get; set; }
+
+        [Name("existence")]
+        public int Existence { get; set; }
+    }
+}
