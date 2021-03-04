9e71ac428dbf3383cdd1661bda782ca777bcc416
SergioCabrera <sergiodcr0258@gmail.com>
2021-03-04 10:48:31 -0600

    Añadiendo singleton y terminando de construir el arbol binario de búsqueda

--- Laboratorio2_ArbolBinario/Services/ABdB.cs
+++ Laboratorio2_ArbolBinario/Services/ABdB.cs
@@ -17,7 +17,31 @@ namespace Laboratorio2_ArbolBinario.Services
         {
             get { return _hijo[i]; }
         }
-
-
+        public ABdBNodo<T> Padre { get; set; }
+        public T Value { get { return _value; } }
+        public ABdBNodo<T> AñadirHijo(T value)
+        {
+            var nodo = new ABdBNodo<T>(value) { Padre = this };
+            _hijo.Add(nodo);
+            return nodo;
+        }
+        public ABdBNodo<T>[] AñadirHijos(params T[] values)
+        {
+            return values.Select(AñadirHijo).ToArray();
+        }
+        public bool QuitarHijo(ABdBNodo<T> nodo)
+        {
+            return _hijo.Remove(nodo);
+        }
+        public void Recorrido(Action<T> action)
+        {
+            action(Value);
+            foreach (var hijo in _hijo)
+                hijo.Recorrido(action);
+        }
+        public IEnumerable<T> Aplanar()
+        {
+            return new[] { Value }.Concat(_hijo.SelectMany(x => x.Aplanar()));
+        }
     }
 }
---
+++ Laboratorio2_ArbolBinario/Services/ISingleton.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Laboratorio2_ArbolBinario.Models;
+
+namespace Laboratorio2_ArbolBinario.Services
+{
+    public interface ISingleton
+    {
+        List<Item> DataSource { get; set; }
+        ABdBNodo<IndexModel> IndexTree { get; set; }
+    }
+}
